9d195879253d0e8bf672a61cf422871ecd1c4d67
Treharne <treharne@liverpool.ac.uk>
2024-01-13 12:56:34 +0000

    first commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'setmeinprod')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-SECRET_KEY="b%r&ui6&(%-xo+&3#04$t*04sw2)3b-04a6$j4=ue6y7fx@bnt"
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
