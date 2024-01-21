330d8125dfd001437855eeae2ef698b22539ecdf
Treharne <treharne@liverpool.ac.uk>
2024-01-21 06:14:05 +0000

    Update SECURE_SSL_REDIRECT setting to True

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,7 +30,7 @@ DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = [] if DEBUG else os.environ.get("DJANGO_ALLOWED_HOSTS").split(',')
 
-SECURE_SSL_REDIRECT = not DEBUG
+SECURE_SSL_REDIRECT = True
 
 
 
