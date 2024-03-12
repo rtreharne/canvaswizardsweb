e6c13af67c50c0a5e8e3ac7fada92ff6d066cff8
Treharne <R.Treharne@liverpool.ac.uk>
2024-03-12 15:39:52 +0000

    xjtlu filtering

--- app/choices/templatetags/filters.py
+++ app/choices/templatetags/filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def filter_by_year(rules, year):
     return [rule for rule in rules if rule.year == year]
+
+@register.filter
+def in_string(value, arg):
+    """Returns True if arg is a substring of value, False otherwise."""
+    return value in arg
