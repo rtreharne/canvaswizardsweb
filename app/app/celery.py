202d5f000bf390b2886fe197a3e91b9e8353bc9e
Treharne <R.Treharne@liverpool.ac.uk>
2024-03-15 09:34:19 +0000

    Fix celery configuration and add report_91.xlsx file

--- app/app/celery.py
+++ app/app/celery.py
@@ -1,10 +1,11 @@
 import os
 from celery import Celery
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # import settings
-from django.conf import settings
 
-os.environ.setdefault(settings, 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 app = Celery('app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
