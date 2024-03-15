a91265397da5a9e6682e1d5256289ecce4076298
Treharne <R.Treharne@liverpool.ac.uk>
2024-03-15 09:54:01 +0000

    Update celery configuration and import statements

--- app/app/celery.py
+++ app/app/celery.py
@@ -1,9 +1,6 @@
 import os
 from celery import Celery
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 app = Celery('app')
