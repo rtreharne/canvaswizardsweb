c22cc418897fbdbfe7aa73ecfc69dcc7d5e278cd
Treharne <R.Treharne@liverpool.ac.uk>
2024-03-15 08:55:43 +0000

    Fix Django settings import in celery.py

--- app/app/celery.py
+++ app/app/celery.py
@@ -1,7 +1,10 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+# import settings
+from django.conf import settings
+
+os.environ.setdefault(settings, 'app.settings')
 app = Celery('app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
