69d3aa76d43991d66ff7ea2502a3b736bbbf1a78
Treharne <treharne@liverpool.ac.uk>
2024-06-12 06:36:07 +0100

    deploy.sh updated to build celery

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,9 @@ docker-compose -f docker-compose-deploy.yml down
 echo "Building the app..."
 docker-compose -f docker-compose-deploy.yml build app
 
+echo "Building celery..."
+docker-compose -f docker-compose-deploy.yml build celery
+
 echo "Bringing up the Docker containers in detached mode..."
 docker-compose -f docker-compose-deploy.yml up -d
 
