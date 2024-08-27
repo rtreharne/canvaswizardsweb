27346861368bb9a7fd2b945078ac11d58b507d23
Treharne <treharne@liverpool.ac.uk>
2024-08-27 21:08:20 +0100

    update certbot

--- docker/certbot/certify-init.sh
+++ docker/certbot/certify-init.sh
@@ -15,6 +15,7 @@ certbot certonly \
     --webroot \
     --webroot-path "/vol/www/" \
     -d "$DOMAIN" \
+    -d "www.$DOMAIN" \
     --email $EMAIL \
     --rsa-key-size 4096 \
     --agree-tos \
