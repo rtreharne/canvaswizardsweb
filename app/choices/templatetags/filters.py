1b8608a7776d477cc2155189a45c889881a58da5
Treharne <treharne@liverpool.ac.uk>
2024-09-30 10:33:45 +0100

    update

--- app/choices/templatetags/filters.py
+++ app/choices/templatetags/filters.py
@@ -2,11 +2,13 @@ from django import template
 
 register = template.Library()
 
-@register.filter
+print("Loading filters")
+
+@register.filter(name='filter_by_year')
 def filter_by_year(rules, year):
     return [rule for rule in rules if rule.year == year]
 
-@register.filter
+@register.filter(name='in_string')
 def in_string(value, arg):
     """Returns True if arg is a substring of value, False otherwise."""
     return value in arg
